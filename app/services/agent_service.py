440e57a3f9ba4ac9ac35860724d96cafaebb90df
iamseyhmus7 <seyhmusok01@gmail.com>
2026-02-21 21:49:06 +0300

    sistemdeki ve kullanıcı verilerinin değişkenlerini tanımladık.

--- app/services/agent_service.py
+++ app/services/agent_service.py
@@ -2,13 +2,19 @@ from agents.graph import build_graph
 
 graph = build_graph()
 
-def run_agent(user_input: str):
+async def run_agent(user_input: str):
+    # LangGraph'in yolda veri kaybetmemesi için TÜM olası anahtarları 
+    # boş veya None olarak en başta tanımlıyoruz. (Kayıt Defteri açmak gibi)
     initial_state = {
         "user_input": user_input,
-        "intent": None,
+        "next_node": None,
+        "search_query": "",
+        "origin": "",
+        "destination": "",
+        "date": "",
         "tool_result": None,
-        "final_answer": None,
+        "final_answer": "",
     }
 
-    result = graph.invoke(initial_state)
-    return result["final_answer"]
+    result = await graph.ainvoke(initial_state)
+    return result.get("final_answer", "Sistemden bir cevap alınamadı.")
