446eb0daa43496cfc7d7c2aee05c7fc3eb395bf5
iamseyhmus7 <seyhmusok01@gmail.com>
2026-02-21 21:46:56 +0300

    Gemini-3-flash modeli korundu.

--- app/core/gemini_client.py
+++ app/core/gemini_client.py
@@ -8,6 +8,6 @@ genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 model = genai.GenerativeModel("gemini-3-flash-preview")
 
-def generate_text(prompt: str):
-    response = model.generate_content(prompt)
+async def generate_text(prompt: str):
+    response = await model.generate_content_async(prompt)
     return response.text
